ac69274a7c5fcc1e544f978afdfa6f082672fa75
seiffawal <83987697+seiffawal@users.noreply.github.com>
2022-08-28 01:49:28 +0200

    PR Done

--- app/src/main/java/com/example/fitnessapp/AddPR.java
+++ app/src/main/java/com/example/fitnessapp/AddPR.java
@@ -3,6 +3,11 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class AddPR extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class AddPR extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pr);
+
+        EditText txtExerciseName = findViewById(R.id.txtExerciseName);
+        EditText txtKgs = findViewById(R.id.txtKgs);
+        EditText txtReps = findViewById(R.id.txtReps);
+        ListView listPR = findViewById(R.id.listPR);
+        Button btnAddNewPR = findViewById(R.id.btnAddNewPR);
+
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listPR.setAdapter(listAdapter);
+
+        btnAddNewPR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String exerciseName = txtExerciseName.getText().toString();
+                String kgs = txtKgs.getText().toString();
+                String reps = txtReps.getText().toString();
+
+                String item = "Exercise: " + exerciseName + "\n" + kgs + " KGs" + "\t" + reps + " reps";
+                listAdapter.add(item);
+
+                txtExerciseName.getText().clear();
+                txtKgs.getText().clear();
+                txtReps.getText().clear();
+            }
+        });
+
     }
 }
