a7936d640f0582dbfd3d09cd9fa660df06392b00
Sharqy <pencilmagicart@gmail.com>
2022-08-27 21:06:33 +0200

    Main Page Done

--- app/src/main/java/com/example/fitnessapp/main_page.java
+++ app/src/main/java/com/example/fitnessapp/main_page.java
@@ -2,7 +2,10 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class main_page extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class main_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        LinearLayout btn_BMI_Calc = findViewById(R.id.btn_BMI_Calc);
+        LinearLayout btn_PR_Tracker = findViewById(R.id.btn_PR_Tracker);
+
+        btn_BMI_Calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(main_page.this, bmi_calc_activity.class));
+            }
+        });
+
+        btn_PR_Tracker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(main_page.this, AddPR.class));
+            }
+        });
     }
 }
